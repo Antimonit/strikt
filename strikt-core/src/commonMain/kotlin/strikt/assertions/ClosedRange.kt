217de210e5b7332f8a5767c84c07bd748ce2d8df
David Khol <david.khol@bucketplace.net>
2026-05-24 23:13:28 +0900

    Fix ClosedRange upper bound generics

--- strikt-core/src/commonMain/kotlin/strikt/assertions/ClosedRange.kt
+++ strikt-core/src/commonMain/kotlin/strikt/assertions/ClosedRange.kt
@@ -5,19 +5,19 @@ import strikt.api.Assertion.Builder
 /**
  * Maps an assertion on the [ClosedRange] to an assertion on its [ClosedRange.start].
  */
-val <T : ClosedRange<E>, E> Builder<T>.start: Builder<E>
+val <T : ClosedRange<E>, E : Comparable<E>> Builder<T>.start: Builder<E>
   get() = get { start }
 
 /**
  * Maps an assertion on the [ClosedRange] to an assertion on its [ClosedRange.endInclusive].
  */
-val <T : ClosedRange<E>, E> Builder<T>.endInclusive: Builder<E>
+val <T : ClosedRange<E>, E : Comparable<E>> Builder<T>.endInclusive: Builder<E>
   get() = get { endInclusive }
 
 /**
  * Asserts that the subject range contains the provided [element].
  */
-infix fun <T : ClosedRange<E>, E> Builder<T>.contains(element: E): Builder<T> =
+infix fun <T : ClosedRange<E>, E : Comparable<E>> Builder<T>.contains(element: E): Builder<T> =
   assert("contains %s", element) {
     when (element) {
       in it -> pass()
@@ -28,7 +28,7 @@ infix fun <T : ClosedRange<E>, E> Builder<T>.contains(element: E): Builder<T> =
 /**
  * Asserts that the subject range is empty.
  */
-fun <T : ClosedRange<E>, E> Builder<T>.isEmpty(): Builder<T> =
+fun <T : ClosedRange<E>, E : Comparable<E>> Builder<T>.isEmpty(): Builder<T> =
   assert("is empty") {
     when {
       it.isEmpty() -> pass()
