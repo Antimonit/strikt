0285190bb2d455624eeab43c931e4a0f0573d8e5
David Khol <david.khol@bucketplace.net>
2026-05-24 23:14:12 +0900

    Fix Kotlin configuration

--- buildLogic/src/main/kotlin/strikt/gradle/commonKotlin.kt
+++ buildLogic/src/main/kotlin/strikt/gradle/commonKotlin.kt
@@ -4,12 +4,9 @@ import org.gradle.kotlin.dsl.assign
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinCommonCompilerOptions
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompilerOptions
-import org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0
 
 fun KotlinCommonCompilerOptions.commonCompilerOptions() {
-  languageVersion.set(KOTLIN_2_0)
-  freeCompilerArgs = listOf("-Xjvm-default=all")
-  allWarningsAsErrors = true
+  this.allWarningsAsErrors = true
 }
 
 fun KotlinJvmCompilerOptions.commonJvmCompilerOptions() {
